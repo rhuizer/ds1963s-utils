78a6372bd5b4e35167afbf8f92a5f0716a2c7bb8
Ronald Huizer <r.huizer@xs4all.nl>
2013-11-11 16:29:12 -0500

    ds1963s-util: Implemented rudimentary DS1963S page signing.
    Signed-off-by: Ronald Huizer <r.huizer@xs4all.nl>

---
+++ src/sha1.h
@@ -0,0 +1,18 @@
+#ifndef __SHA1_INCLUDE
+#define __SHA1_INCLUDE
+
+#include <stdint.h>
+
+#define SHA1_SIGNATURE_SIZE 20
+
+typedef struct {
+    uint32_t state[5];
+    uint32_t count[2];
+    unsigned char buffer[64];
+} SHA1_CTX;
+
+extern void SHA1_Init(SHA1_CTX *);
+extern void SHA1_Update(SHA1_CTX *, const unsigned char *, unsigned int);
+extern void SHA1_Final(unsigned char[SHA1_SIGNATURE_SIZE], SHA1_CTX *);
+
+#endif /* __SHA1_INCLUDE_ */
