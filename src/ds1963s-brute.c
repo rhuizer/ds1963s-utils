103103f7278ef8246c0d09acb24454744811a97a
Ronald Huizer <r.huizer@xs4all.nl>
2013-11-27 20:07:46 -0500

    ds1963s-brute.c: Implemented a rudimentary ncurses interface.
    Signed-off-by: Ronald Huizer <r.huizer@xs4all.nl>

--- src/ds1963s-brute.c
+++ src/ds1963s-brute.c
@@ -1,7 +1,10 @@
 /*
- *  ibutton-brute.c
+ *  ds1963s-brute.c
  *
- *  A complexity reduction attack to uncover secret #0 on the iButton DS1963S.
+ *  A complexity reduction attack to uncover the eight secrets on the DS1963S
+ *  iButton.
+ *
+ *  Dedicated to Yuzuyu Arielle Huizer.
  *
  *  -- Ronald Huizer, 2013
  */
@@ -11,13 +14,18 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <ncurses.h>
 #include <sys/ioctl.h>
 #include "ds1963s.h"
 #include "ds1963s-device.h"
 #include "ibutton/ownet.h"
 #include "ibutton/shaib.h"
 
-#define SERIAL_PORT "/dev/ttyUSB0"
+#define SERIAL_PORT		"/dev/ttyUSB0"
+#define UI_BANNER_HEAD		"DS1963S Brute"
+#define UI_BANNER_TAIL		"Ronald Huizer (C) 2013"
+#define UI_COLUMNS_MIN		80
+#define UI_ROWS_MIN		24
 
 extern int fd[MAX_PORTNUM];
 
@@ -34,6 +42,18 @@ struct brutus
 	struct ds1963s_device	dev;
 	struct brutus_secret	secrets[8];
 };
+
+struct brutus_interface
+{
+	WINDOW		*header;
+	WINDOW		*main;
+	WINDOW		*secrets;
+	int		main_rows;
+	int		main_columns;
+};
+
+/* The main brutus curses interface. */
+struct brutus_interface ui;
 
 int brutus_init_secret(struct brutus *brute, int secret)
 {
@@ -272,29 +292,91 @@ int brutus_do_one(struct brutus *brute, int num)
 	return 1;
 }
 
+struct callback_cookie
+{
+	int			num;
+	struct brutus_secret	*secret;
+	int			phase2_count;
+};
+
+/* Update the progress bar for the hardware attack. */
+NCURSES_WINDOW_CB update_phase1_progress(WINDOW *w, void *cookie)
+{
+	struct callback_cookie *ctx = (struct callback_cookie *)cookie;
+	int piece_count, piece_size = (256 * 4) / 16;
+	char *bar = "                ";
+
+	/* Print the attempted key bytes. */
+	wattron(w, COLOR_PAIR(3));
+	mvwprintw(w, ctx->num * 2 + 1, ctx->secret->secret_idx * 2 + 12,
+	          "%.2x", ctx->secret->secret[ctx->secret->secret_idx]);
+
+	/* Print the progress bar.  It's 16 bytes wide, and we have
+	 * to update it for 256 * 8 attempts in the worst case.
+	 */
+	piece_count = ctx->secret->secret_idx * 256 +
+	              ctx->secret->secret[ctx->secret->secret_idx];
+
+	wattron(w, A_REVERSE | COLOR_PAIR(4));
+	mvwprintw(w, ctx->num * 2 + 2, 12, "%s",
+	          &bar[16 - piece_count / piece_size]);
+	wattroff(w, A_REVERSE);
+	wrefresh(w);
+
+	return 0;
+}
+
+/* Update the progress bar for the software calculation. */
+NCURSES_WINDOW_CB update_phase2_progress(WINDOW *w, void *cookie)
+{
+	struct callback_cookie *ctx = (struct callback_cookie *)cookie;
+	char *bar = "                ";
+	int piece_size;
+
+	piece_size = 0xFFFFFF / 16;
+	wattron(w, A_REVERSE | COLOR_PAIR(5));
+	mvwprintw(w, ctx->num * 2 + 2, 12, "%s",
+	          &bar[16 - ctx->phase2_count / piece_size]);
+	wattroff(w, A_REVERSE);
+	wrefresh(w);
+
+	return 0;
+}
+
+NCURSES_WINDOW_CB finalize_progress(WINDOW *w, void *cookie)
+{
+	struct callback_cookie *ctx = (struct callback_cookie *)cookie;
+	char *bar = "                ";
+
+	/* Update the progress bar for the software calculation. */
+	wattron(w, A_REVERSE | COLOR_PAIR(5));
+	mvwprintw(w, ctx->num * 2 + 2, 12, "%s", bar);
+	wattroff(w, A_REVERSE);
+
+	/* Update the secret as well. */
+	wattron(w, COLOR_PAIR(3));
+	mvwprintw(w, ctx->num * 2 + 1, 22, "%.2x%.2x%.2x",
+		ctx->secret->secret[5], ctx->secret->secret[6],
+		ctx->secret->secret[7]);
+	wrefresh(w);
+
+	return 0;
+}
+
 void brutus_do_secret(struct brutus *brute, int num)
 {
 	struct brutus_secret *secret = &brute->secrets[num];
+	struct callback_cookie cookie = { num, secret, 0 };
+	WINDOW *w = ui.secrets;
 	int i, ret;
 
 	do {
-		printf("\rSecret #%d Trying: [", num);
-		for (i = 0; i <= secret->secret_idx; i++)
-			printf("%.2x", secret->secret[i]);
-		for (i = secret->secret_idx + 1; i < 8; i++)
-			printf("  ");
-		printf("]");
-		fflush(stdout);
-
+		update_phase1_progress(w, &cookie);
 		ret = brutus_do_one(brute, num);
 	} while (ret == 1);
 
-	printf("\n");
-
-	if (ret == -1) {
-		fprintf(stderr, "Something went wrong.\n");
+	if (ret == -1)
 		exit(EXIT_FAILURE);
-	}
 
 	/* Calculate the remaining 3 bytes in software. */
 	memcpy(&brute->dev.secret_memory[num * 8], secret->secret, 5);
@@ -305,8 +387,17 @@ void brutus_do_secret(struct brutus *brute, int num)
 
 		ds1963s_dev_read_auth_page(&brute->dev, num);
 
-		if (!memcmp(secret->target_hmac, &brute->dev.scratchpad[8], 20))
+		if (i % (0xFFFFFF / 16) == 0) {
+			cookie.phase2_count = i;
+			update_phase2_progress(w, &cookie);
+		}
+
+		if (!memcmp(secret->target_hmac, &brute->dev.scratchpad[8], 20)) {
+			secret->secret[5] = (i >>  0) & 0xFF;
+			secret->secret[6] = (i >>  8) & 0xFF;
+			secret->secret[7] = (i >> 16) & 0xFF;
 			break;
+		}
 
 		/* We need to erase the scratchpad again, as the device has
 		 * stored the HMAC there.
@@ -314,10 +405,128 @@ void brutus_do_secret(struct brutus *brute, int num)
 		memset(brute->dev.scratchpad, 0xFF, 32);
 	}
 
-	printf("Key: ");
-	for (i = 0; i < 8; i++)
-		printf("%.2x", brute->dev.secret_memory[num * 8 + i]);
-	printf("\n");
+	if (i == 0xFFFFFF)
+		; /* error */
+
+	finalize_progress(w, &cookie);
+}
+
+void brutus_ui_destroy(void)
+{
+	if (ui.main != NULL)
+		endwin();
+}
+
+static void __hmac_hex(char *buf, uint8_t hmac[20])
+{
+	int i;
+
+	for (i = 0; i < 20; i++)
+		sprintf(&buf[i * 2], "%.2x", hmac[i]);
+}
+
+int brutus_ui_secrets_init(struct brutus *brute)
+{
+	char hmac[41];
+	int i, width;
+	WINDOW *w;
+
+	width = ui.main_columns - 4;
+	if ( (w = ui.secrets = subwin(ui.main, 20, width, 2, 2)) == NULL)
+		return -1;
+
+	if (box(ui.secrets, ACS_VLINE, ACS_HLINE) == ERR)
+		return -1;
+
+	for (i = 0; i < 8; i++) {
+		mvwprintw(w, i * 2 + 1, 1, "Secret #%d", i);
+		mvwprintw(w, i * 2 + 1, 11, "[                ]");
+		mvwprintw(w, i * 2 + 2, 11, "[                ]");
+
+		if (wmove(w, i * 2 + 1, 31) == ERR)
+			return -1;
+
+		__hmac_hex(hmac, brute->secrets[i].target_hmac);
+		waddstr(w, hmac);
+	}
+
+	wrefresh(w);
+
+	return 0;
+}
+
+int brutus_ui_init(struct brutus *brute)
+{
+	/* Register the ui teardown on exit. */
+	if (atexit(brutus_ui_destroy) != 0) {
+		fprintf(stderr, "atexit() failed.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Catch errors for older versions of curses. */
+	if ( (ui.main = initscr()) == NULL)
+		return -1;
+
+	getmaxyx(ui.main, ui.main_rows, ui.main_columns);
+
+	/* See if the terminal has the proper dimension. */
+	if (ui.main_rows < UI_ROWS_MIN || ui.main_columns < UI_COLUMNS_MIN)
+		return -1;
+
+	if ( (ui.header = subwin(ui.main, 1, ui.main_columns, 0, 0)) == NULL)
+		return -1;
+
+	if (cbreak() == ERR)
+		return -1;
+
+	if (curs_set(0) == ERR)
+		return -1;
+
+	if (has_colors() == FALSE)
+		printf("CRAP\n");
+
+	/* Initialize the color scheme we'll use. */
+	if (start_color() == ERR)
+		return -1;
+
+	if (init_pair(1, COLOR_YELLOW, COLOR_BLUE) == ERR)
+		return -1;
+
+	if (init_pair(2, COLOR_BLACK, COLOR_CYAN) == ERR)
+		return -1;
+
+	if (init_pair(3, COLOR_WHITE, COLOR_BLUE) == ERR)
+		return -1;
+
+	if (init_pair(4, COLOR_CYAN, COLOR_BLACK) == ERR)
+		return -1;
+
+	if (init_pair(5, COLOR_GREEN, COLOR_BLACK) == ERR)
+		return -1;
+
+	/* Set the colors for the main window. */
+	if (bkgd(COLOR_PAIR(1)) == -1)
+		return -1;
+
+	if (attron(COLOR_PAIR(3)) == -1)
+		return -1;
+
+	/* Set the colors the the header window. */
+	wbkgd(ui.header, COLOR_PAIR(2));
+	wattron(ui.header, COLOR_PAIR(2));
+
+	mvwaddstr(ui.header, 0, 1, UI_BANNER_HEAD);
+	mvwaddstr(ui.header, 0, ui.main_columns - sizeof UI_BANNER_TAIL,
+	          UI_BANNER_TAIL);
+
+	/* Now initialize the secrets sub-window. */
+	if (brutus_ui_secrets_init(brute) == -1)
+		return -1;
+
+	if (refresh() == -1)
+		return -1;
+
+	return 0;
 }
 
 int main(int argc, char **argv)
@@ -325,17 +534,23 @@ int main(int argc, char **argv)
 	struct brutus brute;
 	int i;
 
+	/* Before we initialize the UI, make sure the button is there. */
 	if (brutus_init(&brute) == -1) {
 		brutus_perror("brutus_init()");
 		exit(EXIT_FAILURE);
 	}
 
-	for (i = 0; i < 8; i++) {
-		printf("Secret #%d Target HMAC: ", i);
-		ds1963s_client_hash_print(brute.secrets[i].target_hmac);
-		brutus_do_secret(&brute, i);
+	/* Initialize the UI. */
+	if (brutus_ui_init(&brute) == -1) {
+		fprintf(stderr, "brutus_ui_init() failed.\n");
+		exit(EXIT_FAILURE);
 	}
 
+	/* Work our way through the eight secrets. */
+	for (i = 0; i < 8; i++)
+		brutus_do_secret(&brute, i);
+
+	getch();
 	brutus_destroy(&brute);
 	exit(EXIT_SUCCESS);
 }
